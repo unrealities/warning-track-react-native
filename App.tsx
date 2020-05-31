722bdb67e975e9903b791035172454c8fbd27c63
Tom Szymanski <szymanski.tom@gmail.com>
2020-05-31 13:49:27 -0600

    adding Google font

--- App.tsx
+++ App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { ImageBackground, StyleSheet, Text, View } from 'react-native';
-
-var backgroundImg = 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI3MCIgaGVpZ2h0PSI3MCI+CjxyZWN0IHdpZHRoPSI3MCIgaGVpZ2h0PSI3MCIgZmlsbD0iIzQ0YWEwMCI+PC9yZWN0Pgo8ZyB0cmFuc2Zvcm09InJvdGF0ZSg0NSkiPgo8cmVjdCB3aWR0aD0iOTkiIGhlaWdodD0iMjUiIGZpbGw9IiM1NWQ0MDAiPjwvcmVjdD4KPHJlY3QgeT0iLTUwIiB3aWR0aD0iOTkiIGhlaWdodD0iMjUiIGZpbGw9IiM1NWQ0MDAiPjwvcmVjdD4KPC9nPgo8L3N2Zz4=';
+import { useFonts, Lobster_400Regular } from '@expo-google-fonts/lobster';
 
 export default function App() {
+  let backgroundImg = 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI3MCIgaGVpZ2h0PSI3MCI+CjxyZWN0IHdpZHRoPSI3MCIgaGVpZ2h0PSI3MCIgZmlsbD0iIzQ0YWEwMCI+PC9yZWN0Pgo8ZyB0cmFuc2Zvcm09InJvdGF0ZSg0NSkiPgo8cmVjdCB3aWR0aD0iOTkiIGhlaWdodD0iMjUiIGZpbGw9IiM1NWQ0MDAiPjwvcmVjdD4KPHJlY3QgeT0iLTUwIiB3aWR0aD0iOTkiIGhlaWdodD0iMjUiIGZpbGw9IiM1NWQ0MDAiPjwvcmVjdD4KPC9nPgo8L3N2Zz4=';
+
+  let [fontsLoaded] = useFonts({
+    Lobster_400Regular,
+  });
+
   return (
     <View style={styles.container}>
       <ImageBackground imageStyle={styles.backgroundImg}
                        source={{uri: backgroundImg}}
                        style={styles.background}>
-        <Text>WarningTrack</Text>
+        <Text style={styles.headerTxt}>WarningTrack</Text>
       </ImageBackground>
     </View>
   );
@@ -26,6 +31,12 @@ const styles = StyleSheet.create({
     resizeMode: 'repeat'
   },
   container: {
-    flex: 1
+    flex: 1,
+    justifyContent: 'center'
+  },
+  headerTxt: {
+    color: '#225500',
+    fontFamily: 'Lobster_400Regular',
+    fontSize: 32
   }
 });
