73754b29f591c257c38398438855d7c7e9131069
Tom Szymanski <szymanski.tom@gmail.com>
2020-07-03 11:01:08 -0500

    try using just interface

--- game.ts
+++ game.ts
@@ -1,4 +1,4 @@
-export class Game {
+export interface Game {
     awayScore: number;
     awayTeam: number; // todo: should be a team object
     balls: number;
@@ -10,21 +10,6 @@ export class Game {
     leverageIndex: number;
     outs: number;
     strikes: number;
-
-    constructor(awayTeam: number, awayScore: number, balls: number, base1: boolean, base2: boolean, base3: boolean, 
-                homeTeam: number, homeScore: number, leverageIndex: number, outs: number, strikes: number) {
-        this.awayScore = awayScore
-        this.awayTeam = awayTeam
-        this.balls = balls
-        this.base1= base1
-        this.base2= base2
-        this.base3= base3
-        this.homeScore = homeScore
-        this.homeTeam = homeTeam
-        this.leverageIndex = leverageIndex
-        this.outs = outs
-        this.strikes = strikes
-    }
-
-    /*TODO: convert bases to baseRunner*/
 }
+
+/*TODO: convert bases to baseRunner*/
