20f6efaeb7cdbbb24ab6c3e8668d84287d3d05e8
Tom Szymanski <szymanski.tom@gmail.com>
2020-10-24 08:48:55 -0600

    now the splash screen doesnt work

--- App.tsx
+++ App.tsx
@@ -70,6 +70,7 @@ export default class App extends React.Component {
     } catch (e) {
       console.warn(e);
     } finally {
+      console.log("reached finally");
       async () => {
         await SplashScreen.hideAsync();
       };
@@ -78,6 +79,7 @@ export default class App extends React.Component {
 
   render() {
     if (!this.state.appIsReady) {
+      console.log("app is loading")
       return (
         <AppLoading
           startAsync={this.prepareResources}
