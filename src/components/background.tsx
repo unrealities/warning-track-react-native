5ce3dd1df5cc8244a6b19938948657a4e14393eb
Tom Szymanski <szymanski.tom@gmail.com>
2021-05-02 09:07:45 -0600

    background not working

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -1,13 +1,18 @@
 import * as React from "react"
+import { View } from 'react-native';
+
+import { BackgroundStyles } from '../styles/background';
 
 function Background() {
   return (
-    <svg width={70} height={70}>
-      <path fill="#4a0" d="M0 0h70v70H0z" />
-      <g fill="#55d400">
-        <path d="M0 0l70.004 70.004L52.326 87.68l-70.004-70.003zM35.355-35.355l70.004 70.003L87.68 52.326 17.678-17.678z" />
-      </g>
-    </svg>
+    <View style={BackgroundStyles.container}>
+      <svg width={70} height={70}>
+        <path fill="#4a0" d="M0 0h70v70H0z" />
+        <g fill="#55d400">
+          <path d="M0 0l70.004 70.004L52.326 87.68l-70.004-70.003zM35.355-35.355l70.004 70.003L87.68 52.326 17.678-17.678z" />
+        </g>
+      </svg>
+    </View>
   )
 }
 
---
+++ src/styles/background.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export const BackgroundStyles = StyleSheet.create({
+    container: {
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        resizeMode: 'stretch'
+    },
+});
