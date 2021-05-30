e669b6caf7c662f262d3159eef9b955f2cc45b6d
Tom Szymanski <szymanski.tom@gmail.com>
2021-05-30 06:50:36 -0600

    Add TODO

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
 module.exports = async function(env, argv) {
+  // TODO: figure out chunking to reduce file sizes
   const config = await createExpoWebpackConfigAsync(env, argv);
   return config;
 };
