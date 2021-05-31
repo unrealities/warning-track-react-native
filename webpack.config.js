6b75a3b49793817d5cda7c314183b1dc534f4f07
Tom Szymanski <szymanski.tom@gmail.com>
2021-05-31 08:56:46 -0600

    android still not working

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,68 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+const path = require("path");
+const webpack = require("webpack");
+const { merge } = require("webpack-merge");
+const ManifestPlugin = require("webpack-manifest-plugin");
 
-module.exports = async function(env, argv) {
+module.exports = webpackConfig = async function(env, argv) {
   // TODO: figure out chunking to reduce file sizes
-  const config = await createExpoWebpackConfigAsync(env, argv);
-  return config;
+  const config = await createExpoWebpackConfigAsync(
+    {
+      ...env,
+      offline: false,
+    },
+    argv
+  );
+
+  return merge(config, {
+    plugins: [
+      new ManifestPlugin({
+        fileName: "manifest.json",
+      }),
+      new webpack.HashedModuleIdsPlugin(),
+    ],
+    optimization: {
+      runtimeChunk: "single",
+      splitChunks: {
+        chunks: "all",
+        maxInitialRequests: Infinity,
+        minSize: 0,
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module) {
+              const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
+              return `npm.${packageName.replace("@","")}`
+            },
+          },
+        },
+      },
+    },
+    resolve: {
+      alias: {
+        "react-native": "react-native-web"
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.[jt]sx?$/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              preset: ["babel-preset-expo"],
+              plugins: [
+                "@babel/plugin-proposal-class-properties",
+                "@babel/plugin-transform-modules-commonjs",
+              ],
+              cacheDirectory: true,
+            },
+          },
+          include: [
+            path.resolve("node_modules/react-navigation")
+          ]
+        }
+      ]
+    },
+  })
 };
