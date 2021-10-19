ca7c487a49d7727088af91aaf2b2f09a16d5639e
Tom Szymanski <szymanski.tom@gmail.com>
2021-10-19 08:53:39 -0500

    trying some code

--- App.tsx
+++ App.tsx
@@ -34,6 +34,44 @@ initializeApp({
 
 WebBrowser.maybeCompleteAuthSession();
 
+export const BASE_URL = "http://192.168.1.8:8000/";
+
+export const showErr = (err: string) => {
+  Platform.OS === "ios"
+      ? Alert.alert("Alert", err, [{ text: "OK" }])
+      : ToastAndroid.show(err, ToastAndroid.LONG);
+};
+
+export const firebaseContextInfo = {
+  config: {
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      databaseURL: process.env.DATABASE_URL,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID,
+      appId: process.env.APP_ID,
+      measurementId: process.env.MEASUREMENT_ID,
+  },
+  firebase: {},
+};
+
+export type userType = {
+  name: string;
+  email: string;
+  picture: string;
+};
+
+export type authState = {
+  state: "login" | "logout" | undefined;
+  user?: userType | undefined;
+};
+
+export type authType = {
+  changeAuthState: React.Dispatch<React.SetStateAction<authState>>;
+  authState: authState;
+};
+
 export default class App extends React.Component {
   state = {
     appIsReady: false,
