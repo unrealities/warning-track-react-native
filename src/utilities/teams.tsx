b14707c0b161ebfcc43957a34f59b529e3ef18e8
Tom Szymanski <szymanski.tom@gmail.com>
2022-04-17 13:45:38 -0500

    something

--- src/utilities/teams.tsx
+++ src/utilities/teams.tsx
@@ -1,18 +1,6 @@
 import Team from "./team";
 
 export default function ConvertTeamID(mlbID: number): Team {
-  const teams = require("./team.json");
-  let blankTeam = new Team("N/A", "", 0, 0);
-  for (let team of teams) {
-    if (team.mlb_id == mlbID) {
-      let newTeam = new Team(
-        team.abbreviation,
-        team.hashtag,
-        team.id,
-        team.mlbID,
-      );
-      return newTeam;
-    }
-    return blankTeam;
-  }
+  const jsonTeams = require("./team.json");
+  return jsonTeams[mlbID.toString()] as Team;
 }
