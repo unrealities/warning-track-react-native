0b630850626b1887cbcb0a3551dd73f562105475
Tom Szymanski <szymanski.tom@gmail.com>
2023-04-06 08:36:26 -0500

    trying to add webpack config to fix expo notifications issue

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const createExpoWebpackConfigAsync = require("@expo/webpack-config");
+
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfigAsync(env, argv);
+  config.module.rules.push({
+    test: /.m?js/,
+    resolve: {
+      fullySpecified: false,
+    },
+  });
+  return config;
+};
