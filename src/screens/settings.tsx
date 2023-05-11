e2071cfd88b6ba75174845b1579bef1b4b9559e1
Tom Szymanski <szymanski.tom@gmail.com>
2023-05-11 08:08:50 -0500

    start settings page

--- src/screens/settings.tsx
+++ src/screens/settings.tsx
@@ -1,8 +1,24 @@
+import React, { useState } from "react"
+import { FlatList, View } from "react-native"
+
 import withBackground from "../utilities/background"
 import GoogleLogin from "../components/googleLogin"
 
+const SettingsContainer = () => {
+  const [settings, setSettings] = useState<Settings>(null)
+
+  return (
+    <View>
+      <GoogleLogin /> 
+      <FlatList 
+      data={settings}
+      showsHorizontalScrollIndicator={false} />
+    </View>
+  )
+}
+
 export const SettingsScreen = () => {
   return (
-    withBackground(GoogleLogin)
+    withBackground(SettingsContainer)
   )
 }
