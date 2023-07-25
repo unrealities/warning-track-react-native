ab758a4a055532216a737ffaff7dbd8439f85de3
Tom Szymanski <szymanski.tom@gmail.com>
2023-07-25 09:16:53 -0500

    store user name

--- src/utilities/hooks/localStorage.ts
+++ src/utilities/hooks/localStorage.ts
@@ -1,10 +1,26 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import uuid from 'react-native-uuid'
 
+const name = 'name'
 const userID = 'userID'
+
+async function setName(name: string) {
+  await AsyncStorage.setItem(name, name)
+}
 
 async function setUserID(id: string) {
   await AsyncStorage.setItem(userID, id)
+}
+
+export async function getName() {
+  let n = await AsyncStorage.getItem(name)
+  if (n) {
+    return n
+  } else {
+    let defaultName = 'default name'
+    setName(defaultName)
+    return defaultName
+  }
 }
 
 export async function getUserID() {
