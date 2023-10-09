9a06d43a5d156f63d5aa7578fafec78c1468389f
Tom Szymanski <szymanski.tom@gmail.com>
2023-10-09 12:43:56 -0500

    main page loads

--- metro.config.js
+++ metro.config.js
@@ -14,7 +14,7 @@ module.exports = (async () => {
             babelTransformerPath: require.resolve("react-native-svg-transformer")    
         },    
         resolver: {
-            assetExts: assetExts.filter(ext => ext !== "svg"),
-            sourceExts: [...sourceExts, "svg"]    
+            assetExts: assetExts.filter(ext => ext !== "svg" || "cjs"),
+            sourceExts: [...sourceExts, "svg", "cjs"]    
         }};
 })();
