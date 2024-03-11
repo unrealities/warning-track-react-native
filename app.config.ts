5ebd48f57beea1e494c687f77538958aa3bf5d70
Tom Szymanski <szymanski.tom@gmail.com>
2024-03-11 08:51:01 -0500

    expoConfig is not working as desired

---
+++ app.config.ts
@@ -0,0 +1,85 @@
+import { ExpoConfig, ConfigContext } from 'expo/config';
+
+export default ({ config }: ConfigContext): ExpoConfig => {
+  const myValue: string = "warning-track-backend";
+
+  return {
+    ...config,
+    android: {
+      googleServicesFile: "./google-services.json",
+      icon: "./assets/images/wt.png",
+      package: "co.warningtrack.android",
+      splash: {
+        backgroundColor: "#44aa00",
+        image: "./assets/images/wt_splash.png",
+        resizeMode: "contain"
+      }
+    },
+    assetBundlePatterns: ["assets/images/*"],
+    description: "Mobile version of warningtrack.co",
+    extra: {
+      firebaseApiKey: process.env.EXPO_PUBLIC_FIREBASE_APIKEY || "",
+      firebaseAppId: process.env.EXPO_PUBLIC_FIREBASE_APPID || "",
+      firebaseMeasurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENTID || "",
+      firebaseMessagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDERID || "",
+      firebaseProjectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECTID || "",
+      themoviedb_key: process.env.EXPO_PUBLIC_THEMOVIEDB_APIKEY || "",
+      expoClientId: process.env.EXPO_PUBLIC_CLIENTID_EXPO || "",
+      iosClientId: process.env.EXPO_PUBLIC_CLIENTID_IOS || "",
+      iosURLConfig: process.env.EXPO_PUBLIC_IOS_URLSCHEME || "",
+      webClientId: process.env.EXPO_PUBLIC_CLIENTID_WEB || "",
+    },
+    githubUrl: "https://github.com/unrealities/warning-track-react-native",
+    icon: "./assets/images/wt.png",
+    ios: {
+      bundleIdentifier: "com.unrealities.warningtrackbackend",
+      infoPlist: {
+        CFBundleURLTypes: [
+          {
+            CFBundleURLSchemes: [process.env.CLIENTID_IOS],
+          },
+        ],
+      },
+      googleServicesFile: "./GoogleService-Info.plist",
+      splash: {
+        backgroundColor: "#44aa00",
+        image: "./assets/images/wt_splash.png",
+        resizeMode: "contain"
+      }
+    },
+    name: myValue,
+    platforms: ["ios", "android", "web"],
+    plugins: [
+      [ "expo-router"],
+      [ "@react-native-google-signin/google-signin"],
+      [ "expo-notifications",
+        {
+          "icon": "./assets/images/wt.png",
+          "color": "#ffffff"
+        }
+      ]
+    ],
+    slug: myValue,
+    splash: {
+      backgroundColor: "#44aa00",
+      image: "./assets/images/wt_splash.png",
+      resizeMode: "contain"
+    },
+    version: process.env.EXPO_PUBLIC_PROJECT_VERSION || "1.0.0",
+    web: {
+      config: {
+        firebase: {
+          apiKey: process.env.EXPO_PUBLIC_FIREBASE_APIKEY,
+          authDomain: `${process.env.EXPO_PUBLIC_FIREBASE_PROJECTID}.firebaseapp.com`,
+          databaseURL: `https://${process.env.EXPO_PUBLIC_FIREBASE_PROJECTID}.firebaseio.com`,
+          projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECTID,
+          storageBucket: `${process.env.EXPO_PUBLIC_FIREBASE_PROJECTID}.appspot.com`,
+          messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDERID,
+          appId: process.env.EXPO_PUBLIC_FIREBASE_APPID,
+          measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENTID,
+        },
+        favicon: "./assets/images/wt.png"
+      }
+    },
+  };
+};
--- src/config/firebase.tsx
+++ src/config/firebase.tsx
@@ -1,14 +1,11 @@
-import Constants from 'expo-constants'
-
-console.log(Constants)
-
+// TODO: Do not use EXPO_PUBLIC for firebase secrets
 export const firebaseConfig = {
-    apiKey: Constants?.expoConfig?.extra?.firebaseApiKey,
-    authDomain: `${Constants?.expoConfig?.extra?.firebaseProjectId}.firebaseapp.com`,
-    databaseURL: `https://${Constants?.expoConfig?.extra?.firebaseProjectId}.firebaseio.com`,
-    projectId: Constants?.expoConfig?.extra?.firebaseProjectId,
-    storageBucket: `${Constants?.expoConfig?.extra?.firebaseProjectId}.appspot.com`,
-    messagingSenderId: Constants?.expoConfig?.extra?.firebaseMessagingSenderId,
-    appId: Constants?.expoConfig?.extra?.firebaseAppId,
-    measurementId: Constants?.expoConfig?.extra?.firebaseAppId
+    apiKey: process.env.EXPO_PUBLIC_FIREBASE_APIKEY,
+    authDomain: `${process.env.EXPO_PUBLIC_FIREBASE_PROJECTID}.firebaseapp.com`,
+    databaseURL: `https://${process.env.EXPO_PUBLIC_FIREBASE_PROJECTID}.firebaseio.com`,
+    projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECTID,
+    storageBucket: `${process.env.EXPO_PUBLIC_FIREBASE_PROJECTID}.appspot.com`,
+    messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDERID,
+    appId: process.env.EXPO_PUBLIC_FIREBASE_APPID,
+    measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENTID,
 }
